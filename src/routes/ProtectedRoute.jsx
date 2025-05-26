81660a6ac49aa45b76a260b45998e83c52ffd46e
iboooop <tais.cruz62@gmail.com>
2025-05-26 01:25:32 -0400

    validación ruta
    funciones de verificación de admin

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,7 +1,23 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-export default function ProtectedRoute({ children }) {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/login" />;
+export default function ProtectedRoute({ children, requiredRole }) {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) return <div>Cargando autenticación...</div>;
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  // Si hay un rol requerido y el usuario no lo cumple
+  if (requiredRole !== "any" && user?.tipo !== requiredRole) {
+    console.warn(
+      `Acceso denegado: necesitas tipo ${requiredRole}, pero eres ${user?.tipo}`
+    );
+    return <Navigate to="/home" replace />;
+  }
+
+  return children;
 }
